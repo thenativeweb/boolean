b49faeba93077ee58a2be60af0ddd9840fcb8d53
mr-perseus <38375248+mr-perseus@users.noreply.github.com>
2019-11-01 10:34:56 +0100

    Remove named capture group for Firefox support. (#28)

--- lib/boolean.ts
+++ lib/boolean.ts
@@ -1,6 +1,13 @@
 const boolean = function (value: any): boolean {
   if (typeof value === 'string') {
-    return /^(?<truthy>true|t|yes|y|on|1)$/iu.test(value.trim());
+    // Currently using a RegExp without the named capture group due to missing support for named capture groups in Firefox.
+    // Kangax table: https://kangax.github.io/compat-table/es2016plus/#test-RegExp_named_capture_groups
+    // Issue on Bugzilla: https://bugzilla.mozilla.org/show_bug.cgi?id=1362154
+    // Regex with named capture group
+    // return /^(?<truthy>true|t|yes|y|on|1)$/iu.test(value.trim());
+
+    // eslint-disable-next-line prefer-named-capture-group
+    return /^(true|t|yes|y|on|1)$/iu.test(value.trim());
   }
 
   if (typeof value === 'number') {
