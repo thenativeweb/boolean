d44ed2220e9ead2ce0845d6bfd18b3aa2eddfe78
Thomas Schaaf <schaaf@komola.de>
2020-02-11 12:09:40 +0100

    Update boolean.ts

--- lib/boolean.ts
+++ lib/boolean.ts
@@ -1,6 +1,6 @@
 const boolean = function (value: any): boolean {
   if (typeof value === 'string') {
-    return [ 'true', '1', 'on', 'yes' ].includes(value.trim().toLowerCase());
+    return [ 'true', 't', 'yes', 'y', 'on', '1' ].includes(value.trim().toLowerCase());
   }
 
   if (typeof value === 'number') {
